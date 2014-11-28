e7b87b023d96829d3cc4c9be7418129ae2fa98c8
lainfox <lainfox@gmail.com>
2014-11-28 16:34:50 +0900

    1

--- plugin.rb
+++ plugin.rb
@@ -5,24 +5,27 @@
 # Facebook Video | Post.
 # from https://www.facebook.com/video.php?v=318742168310835
 # to https://www.facebook.com/video/embed?video_id=318742168310835
-# http://www.jumak.net/onebox?url=https%3A%2F%2Fwww.facebook.com%2Fvideo.php%3Fv%3D304306133101197&refresh=false&_=1417155068364
 
-class Onebox::Engine::FacebookOnebox
-  include Onebox::Engine
+Onebox = Onebox
 
-  REGEX = /^https?:\/\/(?:www\.)?(?:m\.)?(?:facebook\.com\/video.php\?v=)(\d+)\S*$/
-  matches_regexp REGEX
-  
-  def to_html
-  	video_id = @url.match(REGEX)[1]
-    
-    <<HTML
+module Onebox
+	module Engine
+		class FacebookVideoOnebox
+			include Engine
+			REGEX = /^https?:\/\/(?:www\.)?(?:m\.)?(?:facebook\.com\/video.php\?v=)(\d+)\S*$/
+		  matches_regexp REGEX
+
+		  def to_html
+        video_id = @url.match(REGEX)[1]
+        video_src = "https://www.facebook.com/video/embed?video_id=#{video_id}"
+        <<HTML
 <div class="fb-video">
-  <iframe src="https://www.facebook.com/video/embed?video_id=#{video_id}" frameborder="0" allowfullscreen></iframe>
-  <p>#{@url}</p>
+  <iframe src="#{video_src}" width="640" height="360" frameborder="0" allowfullscreen></iframe>
+  <p>#{@url}<br>#{video_src}</p>
 </div>
 HTML
-    
-   end
+      end
 
+		end
+	end	
 end
