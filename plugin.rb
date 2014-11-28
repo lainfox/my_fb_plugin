b58ec67c210c69a442e779e20dd95f7db0ce7646
lainfox <lainfox@gmail.com>
2014-11-28 16:47:16 +0900

    ..

--- plugin.rb
+++ plugin.rb
@@ -14,12 +14,7 @@ module Onebox
 			def to_html
 				video_id = @url.match(REGEX)[1]
 				video_src = "https://www.facebook.com/video/embed?video_id=#{video_id}"
-				<<HTML
-<div class="fb-video">
-	<iframe src="#{video_src}" width="640" height="360" frameborder="0" allowfullscreen></iframe>
-	<p>#{@url}<br>#{video_src}</p>
-</div>
-HTML
+				"<iframe src='#{video_src}'' width='640' height='360' frameborder='0' allowfullscreen></iframe><p>#{@url}</p>"
 			end
 
 		end
