b64ed3b3352a5984cf65e8034d7b597ea7b28269
lainfox <lainfox@gmail.com>
2014-11-28 15:48:45 +0900

    to_html

--- plugin.rb
+++ plugin.rb
@@ -5,27 +5,38 @@
 # Facebook Video | Post.
 # from https://www.facebook.com/video.php?v=318742168310835
 # to https://www.facebook.com/video/embed?video_id=318742168310835
+# http://www.jumak.net/onebox?url=https%3A%2F%2Fwww.facebook.com%2Fvideo.php%3Fv%3D304306133101197&refresh=false&_=1417155068364
 
 class Onebox::Engine::FacebookOnebox
   include Onebox::Engine
-  include Onebox::StandardEmbed
 
   matches_regexp /^https?:\/\/(?:www\.)?(?:m\.)?(?:facebook\.com\/video.php\?v\=).+$/
   #matches_regexp /^https?:\/\/(?:www\.)?(?:m\.)?(?:facebook\.com\/(video.php|permalink.php))\/.+$/
 
   def video_id
-  	#	match = uri.path.match(/\/video.php?v=([^\&]+)/)
-		# return match[1] if match && match[1]
-  	if params['v']
-      return params['v']
-    end
+    match = uri.path.match(/\/video.php\?v=([^\&]+)/)
+    return match[1] if match && match[1]
+  	#if params['v']
+    #  return params['v']
+    #end
 
     nil
    rescue
    	return nil  	
   end
 
+
   def to_html
-    html = "<iframe width=\"640\" height=\"360\" src=\"https://www.facebook.com/video/embed?video_id=#{video_id}\" frameborder=\"0\" allowfullscreen></iframe>"
+  	if video_id
+      # Avoid making HTTP requests if we are able to get the video ID from the
+      # URL.
+      html = "<iframe width=\"640\" height=\"360\" src=\"https://www.facebook.com/video/embed?video_id=#{video_id}\" frameborder=\"0\" allowfullscreen></iframe>"
+    else
+      # Fall back to making HTTP requests.
+      html = raw[:html] || ""
+    end
+
+    html
   end
+
 end
