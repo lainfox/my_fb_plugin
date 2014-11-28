8cb9a12114221874d010241770a2773deda53ba0
lainfox <lainfox@gmail.com>
2014-11-28 15:17:31 +0900

    to html change

--- plugin.rb
+++ plugin.rb
@@ -5,9 +5,11 @@
 # Facebook Video | Post.
 # from https://www.facebook.com/video.php?v=318742168310835
 # to https://www.facebook.com/video/embed?video_id=318742168310835
+# http://www.jumak.net/onebox?url=https%3A%2F%2Fwww.facebook.com%2Fvideo.php%3Fv%3D304306133101197&refresh=false&_=1417155068364
 
 class Onebox::Engine::FacebookOnebox
   include Onebox::Engine
+  include Onebox::StandardEmbed
 
   matches_regexp /^https?:\/\/(?:www\.)?(?:m\.)?(?:facebook\.com\/video.php\?v\=).+$/
   #matches_regexp /^https?:\/\/(?:www\.)?(?:m\.)?(?:facebook\.com\/(video.php|permalink.php))\/.+$/
@@ -24,19 +26,7 @@ class Onebox::Engine::FacebookOnebox
    	return nil  	
   end
 
-
   def to_html
-  	if video_id
-      # Avoid making HTTP requests if we are able to get the video ID from the
-      # URL.
-      html = "<iframe width=\"640\" height=\"360\" src=\"https://www.facebook.com/video/embed?video_id=#{video_id}\" frameborder=\"0\" allowfullscreen></iframe>"
-    else
-      # for channel pages
-      html = Onebox::Engine::WhitelistedGenericOnebox.new(@url, @cache, @timeout).to_html
-      return nil unless html
-      html = html.gsub /http:/, 'https:'
-      html = html.gsub /"\/\//, '"https://'
-      html = html.gsub /'\/\//, "'https://"
-    end
+    html = "<iframe width=\"640\" height=\"360\" src=\"https://www.facebook.com/video/embed?video_id=#{video_id}\" frameborder=\"0\" allowfullscreen></iframe>"
   end
 end
