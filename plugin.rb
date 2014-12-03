168c675e0958b0e0127017ef76c034e11c5c67f7
lainfox <lainfox@gmail.com>
2014-12-03 17:04:06 +0900

    revert

--- plugin.rb
+++ plugin.rb
@@ -11,16 +11,17 @@ module Onebox
 			REGEX = /^https?:\/\/(?:www\.)?(?:m\.)?(?:facebook\.com\/video.php\?v=)(\d+)\S*$/
 			matches_regexp REGEX
 
-			def to_html
-				"<div class='fb-post' data-href='#{@url}' data-width='640'><div class='fb-xfbml-parse-ignore'>via <a href='#{@url}'>#{@url}</a></div></div>"
-			end
-
 			# def to_html
-			# 	video_id = @url.match(REGEX)[1]
-			# 	#video_src = "https://www.facebook.com/video/embed?video_id=#{video_id}"
-			# 	video_src = "https://m.facebook.com/video/video.php?v=#{video_id}"
-			# 	"<div class='fb-video'><iframe src='#{video_src}' frameborder='0' allowfullscreen></iframe><p>via <a href='#{@url}'>#{@url}</a></p></div>"
+			# 	# with FB JSSDK
+			# 	"<div class='fb-post' data-href='#{@url}' data-width='640'><div class='fb-xfbml-parse-ignore'>via <a href='#{@url}'>#{@url}</a><br><span class='fb-post-helper'>페이스북 동영상 주소라면 이 영역에 동영상이 자동으로 나타납니다.</span></div></div>"
 			# end
+
+			def to_html
+				video_id = @url.match(REGEX)[1]
+				video_src = "https://www.facebook.com/video/embed?video_id=#{video_id}"
+				#video_src = "https://m.facebook.com/video/video.php?v=#{video_id}"
+				"<div class='fb-video'><iframe src='#{video_src}' frameborder='0' allowfullscreen></iframe><p>via <a href='#{@url}'>#{@url}</a></p></div>"
+			end
 
 		end
 	end	
